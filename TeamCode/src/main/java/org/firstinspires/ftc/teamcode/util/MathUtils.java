4d1f6675163621be8dbd4fe07a7b8a70f29edb47
oaleksander <medvedev.asm@outlook.com>
2022-04-26 12:59:23 +0300

    Add double almost equals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtils.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import static java.lang.Math.abs;
+
 public class MathUtils {
+    private static final double EPSILON = 0.000001d;
+
     public static double sqr(double num) {
         return num * num;
+    }
+
+    public static boolean doubleEquals(double d1, double d2) {
+        return abs(d1 - d2) < EPSILON;
     }
 }
