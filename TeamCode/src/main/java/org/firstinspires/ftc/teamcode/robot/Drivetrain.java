63891b77cbf752c42c693bbdc96201807b2f9345
Aleksandr Medvedev <medvedev.asm@outlook.com>
2022-04-26 16:18:12 +0300

    Add Drivetrain constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Drivetrain.java
@@ -5,6 +5,12 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Drivetrain extends RobotModule {
 
+    public static double TRACKWIDTH_CM = 50;
+    public static double WHEEL_DIAMETER_CM = 10.6;
+    private static final int MOTOR_CPR_NOGEARBOX = 24;
+    private static final double MOTOR_GEARING = 10;
+    public static double MOTOR_CPR = MOTOR_CPR_NOGEARBOX * MOTOR_GEARING;
+
     public Drivetrain(WoENRobot robot) {
         super(robot);
     }
