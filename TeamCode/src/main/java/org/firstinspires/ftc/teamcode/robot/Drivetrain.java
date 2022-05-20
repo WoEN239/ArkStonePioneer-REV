acbbe3bbf79f1bc25e77501010788a2499c56e1b
oaleksander <medvedev.asm@outlook.com>
2022-05-20 16:50:27 +0300

    Add PID to Drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Drivetrain.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import static java.lang.Math.PI;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.util.CoupledDcMotorEx;
+import org.firstinspires.ftc.teamcode.util.PIDVASMotorController;
 import org.firstinspires.ftc.teamcode.util.TimedSender;
 
 import java.util.function.DoubleConsumer;
@@ -16,34 +19,59 @@ import java.util.stream.Stream;
 public class Drivetrain extends RobotModule {
 
     private static final int MOTOR_CPR_NOGEARBOX = 24;
-    private static final double MOTOR_GEARING = 10;
-    public static double TRACKWIDTH_CM = 50;
-    public static double WHEEL_DIAMETER_CM = 10.6;
-    public static double MOTOR_CPR = MOTOR_CPR_NOGEARBOX * MOTOR_GEARING;
-
-    public static DcMotorSimple.Direction LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
-    public static DcMotorSimple.Direction RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE;
-
-    public static DcMotor.ZeroPowerBehavior MOTOR_ZEROPOWERBEHAVIOR = DcMotor.ZeroPowerBehavior.FLOAT;
-
+    private static final double MOTOR_GEARING = 10.0;
+    private static final double TRACK_WIDTH_CM = 33.70002;
+    private static final double WHEEL_DIAMETER_CM = 10.6;
+    private static final double MOTOR_CPR = MOTOR_CPR_NOGEARBOX * MOTOR_GEARING;
+    private static final double ENCODER_TICKS_TO_CM_RATIO = (WHEEL_DIAMETER_CM * PI) / (MOTOR_CPR);
+    private static final double CM_TO_ROTATION_RADIANS_RATIO = 1.0 / TRACK_WIDTH_CM;
+    private static final double MAX_MOTOR_RPM = 6000.0;
+    private static final double MAX_MOTOR_TPS = MOTOR_CPR * MAX_MOTOR_RPM / 60.0;
+    public static volatile DcMotorSimple.Direction LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
+    public static volatile DcMotorSimple.Direction RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE;
+    public static volatile DcMotor.ZeroPowerBehavior MOTOR_ZEROPOWERBEHAVIOR = DcMotor.ZeroPowerBehavior.FLOAT;
+    public static volatile double MOTOR_REFRESH_RATE_HZ = 0.5;
+    public static volatile DrivetrainControlMode DRIVETRAIN_CONTROL_MODE = DrivetrainControlMode.FEEDFORWARD;
+    public static volatile double VELOCITY_KP = 27.0;
+    public static volatile double VELOCITY_KI = 0.5;
+    public static volatile double VELOCITY_KD = .0;
+    public static volatile double VELOCITY_KV = 14.46;
+    public static volatile double VELOCITY_KS = 2400.0;
+    public static volatile double VELOCITY_MAXI = 16536;
     private DcMotorEx leftMotor;
-    private DcMotorEx rightMotor;
-
-    public static double MOTOR_REFRESH_RATE_HZ = 0.5;
-
     private final TimedSender<Double> leftMotorPowerSender = new TimedSender<>(power -> leftMotor.setPower(power), MOTOR_REFRESH_RATE_HZ);
-    private final TimedSender<Double> rightMotorPowerSender = new TimedSender<>(power -> rightMotor.setPower(power), MOTOR_REFRESH_RATE_HZ);
-
     private final DoubleConsumer leftMotorVoltageCompensator = power -> leftMotorPowerSender.trySend(power * robot.batteryVoltageSensor.getKVoltage());
+    private PIDVASMotorController leftMotorController;
+    private DcMotorEx rightMotor;
+    private final TimedSender<Double> rightMotorPowerSender = new TimedSender<>(power -> rightMotor.setPower(power), MOTOR_REFRESH_RATE_HZ);
     private final DoubleConsumer rightMotorVoltageCompensator = power -> rightMotorPowerSender.trySend(power * robot.batteryVoltageSensor.getKVoltage());
-
+    private PIDVASMotorController rightMotorController;
+    private double forwardVelocity = 0.0;
+    private double turnVelocity = 0.0;
 
     public Drivetrain(WoENRobot robot) {
         super(robot);
     }
 
-    private double forwardVelocity = 0.0;
-    private double turnVelocity = 0.0;
+    private double cmToEncoderTicks(double cm) {
+        return cm / ENCODER_TICKS_TO_CM_RATIO;
+    }
+
+    private double encoderTicksToCm(double ticks) {
+        return ticks * ENCODER_TICKS_TO_CM_RATIO;
+    }
+
+    private double encoderTicksToRotationRadians(double ticks) {
+        return encoderTicksToCm(ticks) * CM_TO_ROTATION_RADIANS_RATIO;
+    }
+
+    private double motorEncodersToDistance(double leftTicks, double rightTicks) {
+        return encoderTicksToCm((leftTicks + rightTicks) * 0.5);
+    }
+
+    private double motorEncodersToRotation(double leftTicks, double rightTicks) {
+        return encoderTicksToRotationRadians((-leftTicks + rightTicks) * 0.5);
+    }
 
     public void setRawPower(double rawForwardPower, double rawTurnPower) {
         this.forwardVelocity = rawForwardPower;
@@ -63,11 +91,25 @@ public class Drivetrain extends RobotModule {
         allMotors.get().forEach(dcMotor -> dcMotor.setZeroPowerBehavior(MOTOR_ZEROPOWERBEHAVIOR));
         allMotors.get().forEach(dcMotor -> dcMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER));
         allMotors.get().forEach(dcMotor -> dcMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER));
+
+        leftMotorController = new PIDVASMotorController(leftMotorVoltageCompensator, leftMotor::getVelocity,
+                () -> VELOCITY_KP, () -> VELOCITY_KI, () -> VELOCITY_KD, () -> VELOCITY_KV, () -> 0, () -> VELOCITY_KS, () -> VELOCITY_MAXI, 0, false);
+        rightMotorController = new PIDVASMotorController(leftMotorVoltageCompensator, rightMotor::getVelocity,
+                () -> VELOCITY_KP, () -> VELOCITY_KI, () -> VELOCITY_KD, () -> VELOCITY_KV, () -> 0, () -> VELOCITY_KS, () -> VELOCITY_MAXI, 0, false);
     }
 
     @Override
     public void update() {
-        leftMotorVoltageCompensator.accept(forwardVelocity - turnVelocity);
-        rightMotorVoltageCompensator.accept(forwardVelocity + turnVelocity);
+        if (DRIVETRAIN_CONTROL_MODE == DrivetrainControlMode.FEEDFORWARD) {
+            leftMotorVoltageCompensator.accept(forwardVelocity - turnVelocity);
+            rightMotorVoltageCompensator.accept(forwardVelocity + turnVelocity);
+        } else {
+            leftMotorController.update(MAX_MOTOR_TPS * (forwardVelocity - turnVelocity));
+            rightMotorController.update(MAX_MOTOR_TPS * (forwardVelocity + turnVelocity));
+        }
+    }
+
+    public enum DrivetrainControlMode {
+        FEEDFORWARD, PIDVAS
     }
 }
